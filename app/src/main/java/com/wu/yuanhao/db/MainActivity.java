ffeba5b39c78a931173dc000a4c17ed3c63e2996
Yuanhao WU <yuanhao_wu@yeah.net>
2018-04-23 21:10:49 +0800

    Start to add HomePage, which should be initiated by clicking "Login".

--- app/src/main/java/com/wu/yuanhao/db/MainActivity.java
+++ app/src/main/java/com/wu/yuanhao/db/MainActivity.java
@@ -1,27 +1,49 @@
 package com.wu.yuanhao.db;
 // http://www.cnblogs.com/lonelyxmas/p/7349176.html
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mNameEt = null; // 用户名输入
+    private EditText mPasswordEt = null; // 密码输入
+    private Button mLoginBtn = null; // 登录按钮
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // 获取界面组件
+        mNameEt = (EditText) findViewById(R.id.name_et);
+        mPasswordEt = (EditText) findViewById(R.id.pwd_et);
+        mLoginBtn = (Button) findViewById(R.id.login_btn);
+
+        // 登录按钮绑定点击事件
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // 获取用户名和密码的输入
+                String name = mNameEt.getText().toString();
+                String password = mPasswordEt.getText().toString();
+                String testPwd = "abc123";
+
+                // 消息提示
+                if( password == testPwd ){
+                    // TODO
+                    Intent intent = new Intent(MainActivity.this, homePageActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this,
+                            "用户名密码错误", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
